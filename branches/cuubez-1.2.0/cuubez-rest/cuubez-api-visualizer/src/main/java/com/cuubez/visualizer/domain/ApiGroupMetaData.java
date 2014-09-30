827f9746652e632590d766d6c1075d15b88776c6
prabathmail <prabathmail@e12a6ff3-6c03-e055-6f88-9e55e6ff6be0>
2014-09-30 17:46:12 +0000

    code refactoring

--- branches/cuubez-1.2.0/cuubez-rest/cuubez-api-visualizer/src/main/java/com/cuubez/visualizer/domain/ApiGroupMetaData.java
+++ branches/cuubez-1.2.0/cuubez-rest/cuubez-api-visualizer/src/main/java/com/cuubez/visualizer/domain/ApiGroupMetaData.java
@@ -19,9 +19,18 @@ import java.util.List;
 
 public class ApiGroupMetaData {
 
+    public String id;
     public String groupName;
     public String groupTittle;
     public List<ApiMetaData> apiMetaDataList;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getGroupName() {
         return groupName;
